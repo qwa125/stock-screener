0738e39b49eb07a97956ad57f9dac5ac802cc7f3
qwa125 <qwa125@users.noreply.github.com>
2026-06-28 21:48:47 +0800

    chore: 推送触发Render部署
    Coze-Commit-Type: user
    Coze-User-ID: 4351180157764635
    Coze-Conversation-ID: 7327372

--- server/dist/modules/stock/xing-xing.d.ts
+++
@@ -1,3 +0,0 @@
-import { FormulaEngine } from './formula-engine';
-import { FormulaResult } from './types';
-export declare function calcXingXing(engine: FormulaEngine): Pick<FormulaResult, 'jiGouHuoYueDu' | 'breakLifeLine' | 'breakStrongLine' | 'breakBigBullLine' | 'jiGouHuoYueDuArray'>;
--- server/dist/version.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BUILD_ID = void 0;
-exports.BUILD_ID = '1781179107';
