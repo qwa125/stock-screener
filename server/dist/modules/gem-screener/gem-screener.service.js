0ad81fbe48d798a41c60cc7602e91eeba8be5936
qwa125 <qwa125@users.noreply.github.com>
2026-06-30 22:39:44 +0800

    fix: tsconfig添加esModuleInterop修复postgres导入

--- server/dist/modules/gem-screener/gem-screener.service.js
+++ server/dist/modules/gem-screener/gem-screener.service.js
@@ -1,12 +1,48 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 var GemScreenerService_1;
 Object.defineProperty(exports, "__esModule", { value: true });
@@ -14,12 +50,12 @@ exports.GemScreenerService = void 0;
 const common_1 = require("@nestjs/common");
 const coze_coding_dev_sdk_1 = require("coze-coding-dev-sdk");
 const pinyin_pro_1 = require("pinyin-pro");
-const postgres_1 = require("postgres");
+const postgres_1 = __importDefault(require("postgres"));
 const node_fs_1 = require("node:fs");
 const node_path_1 = require("node:path");
 const trading_suggestion_1 = require("../../utils/trading-suggestion");
 const market_time_1 = require("../../utils/market-time");
-const data_1 = require("../../industry-sectors/data");
+const data_1 = __importStar(require("../../industry-sectors/data"));
 const data_fetcher_service_1 = require("../stock/data-fetcher.service");
 const stock_service_1 = require("../stock/stock.service");
 const technical_analysis_1 = require("./technical-analysis");
